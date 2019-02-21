794ecbba64d985b4ba06d3af05d28eeb260b88aa
Spike Grobstein <me@spike.cx>
2019-02-20 18:20:27 -0800

    wip: use a db directory

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 mod alamo_movies;
 use crate::alamo_movies::cinema::Cinema;
 
+use std::env;
+
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let cinema_id = args.get(1).expect("Please supply a cinema_id");
+
     // first, read the file into a string
-    let path = "0002.calendar.json";
+    let mut path = String::from("./db/");
+    path.push_str(cinema_id);
+    path.push_str(".calendar.json");
 
     let cinema = Cinema::from_calendar_file(&path).expect("cannot load file");
 
